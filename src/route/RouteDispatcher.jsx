ebf413b0654927b1d04d77d88cef606394bc16cf
HongdaLin <67817916+Hongda-OSU@users.noreply.github.com>
2023-11-28 10:01:54 -0600

    login finished (#1)

--- src/route/RouteDispatcher.jsx
+++ src/route/RouteDispatcher.jsx
@@ -1,11 +1,26 @@
-import { BrowserRouter, Routes, Route,  } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthState } from "../utilities/firebaseUtils";
 import MindNetLogin from "../components/MindNetLogin/MindNetLogin";
+import MindNetHome from "../components/MindNetHome/MindNetHome";
 
 const RouteDispatcher = () => {
+  const [user] = useAuthState();
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MindNetLogin />} />
+        <Route
+          path="/"
+          element={
+            user ? (
+              <Navigate replace to="/nu-mindnet-home" />
+            ) : (
+              <Navigate replace to="/nu-mindnet-login" />
+            )
+          }
+        />
+        <Route path="/nu-mindnet-login" element={<MindNetLogin />} />
+        <Route path="/nu-mindnet-home" element={<MindNetHome />} />
       </Routes>
     </BrowserRouter>
   );
