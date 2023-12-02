bb15d4b4c5583d1099328efd94c2ef2e47267c2c
vivian1101 <zhang3152191709@gmail.com>
2023-12-02 01:55:33 -0600

    Updated_Questionnaire

--- src/components/pages/Questionnaire.jsx
+++ src/components/pages/Questionnaire.jsx
@@ -1,29 +1,22 @@
 import React, { useState } from "react";
-import {
-  Container,
-  TextField,
-  Button,
-  Typography,
-  Box,
-  RadioGroup,
-  FormControlLabel,
-  Radio,
-  FormControl,
-  FormLabel,
-  List,
-  ListItem,
-} from "@mui/material";
+import { Container, Box } from "@mui/material";
+
+import PersonInfo from "../Questionnaire/PersonInfo";
+import Questions from "../Questionnaire/Questions";
 
 const Questionnaire = () => {
   const [step, setStep] = useState(1);
-  const [currentPersonIndex, setCurrentPersonIndex] = useState(0);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [people, setPeople] = useState([
     { name: "", gender: "", age: "" },
     { name: "", gender: "", age: "" },
     { name: "", gender: "", age: "" },
+    { name: "", gender: "", age: "" },
   ]);
-  const [userName, setUserName] = useState("");
-  const [userGender, setUserGender] = useState("");
+  const questions = [
+    "Who do you think this person will most likely collaborate with on a project?",
+    "Which of the following people do you think this person would choose to travel with?"
+  ];
 
   const handlePersonInputChange = (index, name, value) => {
     const newPeople = [...people];
@@ -34,12 +27,25 @@ const Questionnaire = () => {
   const handleAddPerson = () => {
     setPeople([...people, { name: "", gender: "", age: "" }]);
   };
+  const handleDeletePerson = () => {
+    // Check if the array is not empty
+    if (people.length > 4) {
+      // Create a new array without the last element
+      const newPeople = people.slice(0, -1);
+      // Update the state with the new array
+      setPeople(newPeople);
+    }
+  };
 
   const handleNext = () => {
-    if (userName.trim() && userGender) {
-      setStep(2);
+    const isAllFieldsFilled = people.every(person => 
+      person.name.trim() && person.gender.trim() && person.age.toString().trim()
+    );
+  
+    if (isAllFieldsFilled) {
+      setStep(2); // Go to the next step if every person has all fields filled
     } else {
-      alert("Please enter your name and select your gender.");
+      alert("Please enter all the fields for each person.");
     }
   };
 
@@ -55,15 +61,12 @@ const Questionnaire = () => {
       return;
     }
     const data = {
-      userName,
-      userGender,
       people: filteredPeople,
     };
     const jsonData = JSON.stringify(data);
     console.log(jsonData);
     // Here you would send jsonData to your backend API
   };
-
   return (
     <Container component="main" maxWidth="lg">
       <Box
@@ -75,159 +78,24 @@ const Questionnaire = () => {
         }}
       >
         {step === 1 && (
-          <Box sx={{ maxWidth: 500 }}>
-            <Typography component="h1" variant="h5">
-              General Questions
-            </Typography>
-            <TextField
-              label="What is your name?"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-              margin="normal"
-              fullWidth
-            />
-            <FormControl component="fieldset" margin="normal">
-              <FormLabel component="legend">What is your gender?</FormLabel>
-              <RadioGroup
-                row
-                value={userGender}
-                onChange={(e) => setUserGender(e.target.value)}
-              >
-                <FormControlLabel
-                  value="female"
-                  control={<Radio />}
-                  label="Female"
-                />
-                <FormControlLabel
-                  value="male"
-                  control={<Radio />}
-                  label="Male"
-                />
-                <FormControlLabel
-                  value="other"
-                  control={<Radio />}
-                  label="Other"
-                />
-              </RadioGroup>
-            </FormControl>
-            <Typography component="p" variant="body1" sx={{ mt: 2 }}>
-              Please fill in the list below (Min of 3 is required).
-            </Typography>
-            {people.map((person, index) => (
-              <TextField
-                key={index}
-                label={`Person ${index + 1} Name`}
-                value={person.name}
-                onChange={(e) =>
-                  handlePersonInputChange(index, "name", e.target.value)
-                }
-                margin="normal"
-                fullWidth
-              />
-            ))}
-            <Box
-              sx={{ display: "flex", justifyContent: "space-between", mt: 2 }}
-            >
-              <Button onClick={handleAddPerson} variant="contained">
-                Add Person
-              </Button>
-              <Button onClick={handleNext} variant="contained" color="primary">
-                Next
-              </Button>
-            </Box>
-          </Box>
+          <PersonInfo
+            people={people}
+            handleNext={handleNext}
+            handleAddPerson={handleAddPerson}
+            handleDeletePerson={handleDeletePerson}
+            handlePersonInputChange={handlePersonInputChange}
+          />
         )}
 
         {step === 2 && (
-          <React.Fragment>
-            <Box
-              sx={{
-                width: "200px",
-                marginRight: "2rem",
-                maxHeight: "calc(100vh - 100px)",
-                overflow: "auto",
-              }}
-            >
-              <Typography variant="h6">People</Typography>
-              <List>
-                {people.map((person, index) => (
-                  <ListItem
-                    key={index}
-                    button
-                    selected={index === currentPersonIndex}
-                    onClick={() => setCurrentPersonIndex(index)}
-                  >
-                    {person.name || `Person ${index + 1}`}
-                  </ListItem>
-                ))}
-              </List>
-            </Box>
-            <Box sx={{ flex: 1 }}>
-              <Typography component="h2" variant="h5">
-                Details for{" "}
-                {people[currentPersonIndex].name ||
-                  `Person ${currentPersonIndex + 1}`}
-              </Typography>
-              <FormControl component="fieldset" margin="normal">
-                <FormLabel component="legend">Gender</FormLabel>
-                <RadioGroup
-                  row
-                  name="gender"
-                  value={people[currentPersonIndex].gender}
-                  onChange={(e) =>
-                    handlePersonInputChange(
-                      currentPersonIndex,
-                      "gender",
-                      e.target.value
-                    )
-                  }
-                >
-                  <FormControlLabel
-                    value="female"
-                    control={<Radio />}
-                    label="Female"
-                  />
-                  <FormControlLabel
-                    value="male"
-                    control={<Radio />}
-                    label="Male"
-                  />
-                  <FormControlLabel
-                    value="other"
-                    control={<Radio />}
-                    label="Other"
-                  />
-                </RadioGroup>
-              </FormControl>
-              <TextField
-                label="Age"
-                type="number"
-                name="age"
-                value={people[currentPersonIndex].age}
-                onChange={(e) =>
-                  handlePersonInputChange(
-                    currentPersonIndex,
-                    "age",
-                    e.target.value
-                  )
-                }
-                margin="normal"
-                fullWidth
-              />
-            </Box>
-          </React.Fragment>
-        )}
-      </Box>
-      <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}>
-        {step === 2 && (
-          <Button onClick={handleBack} variant="contained" sx={{ mr: 2 }}>
-            Go Back
-          </Button>
-        )}
-        {currentPersonIndex === people.length - 1 && (
-          <Button onClick={handleSubmit} variant="contained">
-            Submit
-          </Button>
+          <Questions
+            people={people}
+            questions={questions}
+            currentQuestionIndex={currentQuestionIndex}
+            setCurrentQuestionIndex={setCurrentQuestionIndex}
+            handleBack={handleBack}
+            handleSubmit={handleSubmit}
+          />
         )}
       </Box>
     </Container>
