0a9d2354222be47acd759646c61875236cb20580
VivianZhang1101 <91089562+VivianZhang1101@users.noreply.github.com>
2023-12-01 19:02:39 -0600

    questionnaire and Homepage #1 (#2)
    * test
    
    * Homepage and Questionnaire (V1)
    
    * questionnaire and Homepage #2

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import { firebaseSignOut } from "../../utilities/firebaseUtils";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const signout = () => {
+    firebaseSignOut()
+      .then(() => {
+        navigate("/nu-mindnet-login");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <AppBar position="static">
+      <Toolbar style={{ justifyContent: "space-between" }}>
+        <Button color="inherit" onClick={() => navigate("/nu-mindnet-home")}>
+          <Typography variant="h6" component="div">
+            NU MindNet
+          </Typography>
+        </Button>
+        <Button color="inherit" onClick={signout}>
+          Log out
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
